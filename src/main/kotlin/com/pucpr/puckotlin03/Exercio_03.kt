7317bd8e0f9aa355da39e98a71bf983cf4c21a16
Raison Robert Monteiro de Sá <raison.robert@pucpr.edu.br>
2022-08-24 14:00:43 -0400

    Regex
    para fazer o mimificador tive que usar o Regex por Intercessão

--- src/main/kotlin/com/pucpr/puckotlin03/Exercio_03.kt
+++ src/main/kotlin/com/pucpr/puckotlin03/Exercio_03.kt
@@ -1,21 +1,19 @@
 package com.pucpr.puckotlin03
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api")
 class Exercio_03 {
     //Criar um Mimificador
-    @RequestMapping("mimificador")
+    @RequestMapping("mimificador/{text}")
     @ResponseBody
-    fun number(
-        @RequestParam
-            (name = "mimi", required = false, defaultValue = "get") mimi: String
-    ): String {
-        
-        return mimi
+    fun handleString(@PathVariable text: String): String {
+        val padraEsperado = """(a|á|à|ã|â|ä|e|ë|é|è|ê|ï|i|í|ì|î|o|ö|ô|ó|ò|õ|ü|u|ú|ù|û)""".toRegex()
+        val result = padraEsperado.replace(text,"i")
+        return result
     }
 }
+//        val padraoEsperado = "([0-9]{2})([0-9]{4,5})([0-9]{4})".toRegex()
+//        val formatoEsperado = "($1) $2-$3"
+//        val telefoneFormatado = "1122223333".replace(padraoEsperado, formatoEsperado)
